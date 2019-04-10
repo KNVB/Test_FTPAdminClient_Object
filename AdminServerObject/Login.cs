eba7dea1898acc7588ae25cdec8cb94d014a73e5
HKO Helpdesk <helpdesk@hko.gov.hk>
2019-04-10 12:26:57 +0800

    Restructure the solution

---
+++ AdminServerObject/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLibrary
+{
+    class Login
+    {
+        public string action="Login";
+		public string userName { get; set; }
+		public string password{ get; set; }
+    }
+}
