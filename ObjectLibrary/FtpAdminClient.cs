1f7c8a1266278dd587587a959a7c65d83ba15fb0
HKO Helpdesk <helpdesk@hko.gov.hk>
2019-04-10 10:38:59 +0800

    FtpAdminClient object added

---
+++ ObjectLibrary/FtpAdminClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLibrary
+{
+    public class FtpAdminClient
+    {
+        internal SortedDictionary<string, AdminServer> adminServerList = new SortedDictionary<string, AdminServer>();
+        internal string lastServerKey = "";
+        public FtpAdminClient()
+        {
+
+        }
+        internal void disconnectServer(string key)
+        {
+            AdminServer adminServer;
+            adminServer = adminServerList[key];
+            adminServer.disConnect();
+            adminServerList.Remove(key);
+        }
+        internal void disconnectAllAdminServer()
+        {
+            string[] keys = adminServerList.Keys.ToArray();
+            for (int i = 0; i < keys.Length; i++)
+            {
+                disconnectServer(keys[i]);
+            }
+        }
+
+        internal int addRemoteServer(string adminServerName, int adminPortNo, string adminUserName, string adminUserPassword)
+        {
+            int result = 0;
+            try
+            {
+                if (adminServerList.ContainsKey(adminServerName + ":" + adminPortNo))
+                {
+                    result = 1;
+                }
+                else
+                {
+                    AdminServer adminServer = new AdminServer();
+                    if (adminServer.connect(adminServerName, adminPortNo))
+                    {
+                        if (adminServer.login(adminUserName, adminUserPassword))
+                        {
+                            adminServerList.Add(adminServerName + ":" + adminPortNo, adminServer);
+                            lastServerKey = adminServerName + ":" + adminPortNo;
+                        }
+                        else
+                            result = 3;
+                    }
+                    else
+                    {
+                        result = 2;
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                throw new Exception("An error occurs when login to admin. server:" + err.Message);
+            }
+            return result;
+        }
+    }
+}
